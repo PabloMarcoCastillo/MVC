befc359a9cd1322eae4a6d633676aac9c729b194
Pablo Marco <pablomarcocastillo@gmail.com>
2020-10-28 17:07:26 +0000

    mvc

---
+++ views/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Usuarios</title>
+</head>
+<body>
+<h1>Lista de usuarios</h1>
+<table>
+    
+    <?php
+    foreach ($users as $user) {
+        echo "<tr>";
+        echo "<td> Usuario $user[0] </td>";
+        echo "<td> $user[1] </td>";
+        echo "<td> <a href='index.php?method=show&id=$user[0]'> ver </a></td>";
+        echo "</tr>";
+    }
+    ?>
+
+    </table>
+</body>
+</html>
+
+
+
